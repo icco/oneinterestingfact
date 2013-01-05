26f57d4de5b398dc33c2058f383a3797ffef1435
Nat Welch <nat@natwelch.com>
2013-01-05 15:52:35 -0800

    comment formatting.

--- app/controllers.rb
+++ app/controllers.rb
@@ -1,13 +1,13 @@
 Onefact.controllers  do
   layout :main
 
-  # * /
+  # /
   #   * Signup, Gameplay
   get :index do
     render :index
   end
 
-  # * /login
+  # /login
   #   * sign in w/ email
   get :login do
     render :login
@@ -17,29 +17,29 @@ Onefact.controllers  do
     404
   end
 
-  # * /game
+  # /game
   #   * start new game
   #   * status of all current games
   get :game do
     render :games
   end
 
-  # * /game/:id
-  #   * if new game
-  #     * invite players
-  #   * else if chooser
-  #     * status of submitters
-  #     * time left
-  #     * once everyone has submitted or time up Make a choice
-  #   * else
-  #     * status of submitters (1 of x has submitted)
-  #     * submit your post
+  # /game/:id
   get :game, :with => :id do
+    # if new game
+    # * invite players
+    # else if chooser
+    # * status of submitters
+    # * time left
+    # * once everyone has submitted or time up Make a choice
+    # else
+    # * status of submitters (1 of x has submitted)
+    # * submit your post
     @game = Game.get(params[:id])
     render :game
   end
 
-  # * /user/:name
+  # /user/:name
   #   * win stats
   #   * games played stats
   #   * "I've played games with"
