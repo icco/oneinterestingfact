519b3c4ad5e8025803116ae1c451490120e2da55
Nat Welch <nat@natwelch.com>
2013-01-06 23:00:01 -0800

    define reg email

--- app/mailers.rb
+++ app/mailers.rb
@@ -10,29 +10,13 @@
 #   via     :sendmail              # optional, to smtp if defined, otherwise sendmail
 #   render  'registration_email'
 # end
-#
-# You can set the default delivery settings from your app through:
-#
-#   set :delivery_method, :smtp => {
-#     :address         => 'smtp.yourserver.com',
-#     :port            => '25',
-#     :user_name       => 'user',
-#     :password        => 'pass',
-#     :authentication  => :plain, # :plain, :login, :cram_md5, no auth by default
-#     :domain          => "localhost.localdomain" # the HELO domain provided by the client to the server
-#   }
-#
-# or sendmail (default):
-#
-#   set :delivery_method, :sendmail
-#
-# or for tests:
-#
-#   set :delivery_method, :test
-#
-# and then all delivered mail will use these settings unless otherwise specified.
-#
-
 QuizPopsicle.mailer :notifier do
-  # Message definitions here...
+  email :registration_email do |user|
+    from 'nat@quizpopsicle.com'
+    to   user.email
+    subject 'Welcome to the site!'
+    locals  :name => name
+    content_type 'text/html'       # optional, defaults to plain/text
+    render  'registration_email'
+  end
 end
