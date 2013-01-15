1c4b3356870d175458ebf0898ad4a3517fc26ae2
Nat Welch <nat@natwelch.com>
2013-01-14 22:55:17 -0800

    Fix user bug

--- models/user.rb
+++ models/user.rb
@@ -22,8 +22,8 @@ class User < ActiveRecord::Base
   end
 
   def generate_password size = 12
-    CHARSET = %w{ 2 3 4 6 7 9 A C D E F G H J K M N P Q R T V W X Y Z a c d e f g h j k m n p q r t v w x y z}
-    pw = (0...size).map{ charset.to_a[rand(charset.size)] }.join
+    @@charset = %w{ 2 3 4 6 7 9 A C D E F G H J K M N P Q R T V W X Y Z a c d e f g h j k m n p q r t v w x y z}
+    pw = (0...size).map{ @@charset.to_a[rand(@@charset.size)] }.join
     self.password = pw
     return true
   end
